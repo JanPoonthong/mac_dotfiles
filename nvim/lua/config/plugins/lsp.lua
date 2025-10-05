16f4f6a47d678451ae239780646053c4c8556e11
Jan Poonthong <janpoonthong628@gmail.com>
2025-10-05 07:25:38 +0700

    something

--- nvim/lua/config/plugins/lsp.lua
+++ nvim/lua/config/plugins/lsp.lua
@@ -1,15 +1,26 @@
 return {
     {
         "neovim/nvim-lspconfig",
-        dependences = 'saghen/blink.cmp',
+        dependencies = 'saghen/blink.cmp',
         config = function()
             local capabilities = require('blink.cmp').get_lsp_capabilities()
-            local lspconfig = require('lspconfig')
-            lspconfig.pyright.setup{capabilites = capabilities}
-            lspconfig.lua_ls.setup{capabilites = capabilities}
-            lspconfig.ts_ls.setup{capabilites = capabilities}
 
+            -- Lua
+            vim.lsp.config('lua_ls', { capabilities = capabilities })
+            vim.lsp.enable('lua_ls')
+
+            -- Python
+            vim.lsp.config('pyright', { capabilities = capabilities })
+            vim.lsp.enable('pyright')
+
+            -- TypeScript / JavaScript
+            vim.lsp.config('ts_ls', { capabilities = capabilities })
+            vim.lsp.enable('ts_ls')
+
+            -- Keymap for hover
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+
+            -- Diagnostics config
             vim.diagnostic.config({
                 virtual_text = true,
                 signs = true,
@@ -33,7 +44,7 @@ return {
         config = function()
             require("mason").setup()
             vim.keymap.set("n", "<leader>s", function()
-              vim.lsp.buf.format()
+                vim.lsp.buf.format()
             end, { desc = "Format file" })
         end
     }
